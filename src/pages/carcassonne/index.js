b0665601b4a8f7b765d1aba8802ebd2f9a28c7e1
hung <quochung@simicart.com>
2023-02-09 01:30:11 +0700

    update

--- src/pages/carcassonne/index.js
+++ src/pages/carcassonne/index.js
@@ -4,14 +4,15 @@ import Map from './map';
 const Carcassonne = (props) => {
     return (
         <div className="pt-2 pb-2">
-            <h1 className="text-center max-w-lg mx-auto">
-                <img className="h-auto mx-auto" src="/images/carcassonne/splash.png" />
-            </h1>
-            <div class="flex flex-row flex-wrap py-4">
-                <main role="main" class="w-full sm:w-2/3 md:w-3/4 pt-1 px-2">
+            <div className="flex flex-row flex-wrap py-4">
+                <main role="main" className="w-full sm:w-2/3 md:w-3/4 pt-1 px-2">
                     <Map />
                 </main>
-                <aside class="w-full sm:w-1/3 md:w-1/4 px-2"></aside>
+                <aside className="w-full sm:w-1/3 md:w-1/4 px-2">
+                    <h1 className="text-center max-w-lg mx-auto">
+                        <img className="h-auto mx-auto" src="/images/carcassonne/splash.png" />
+                    </h1>
+                </aside>
             </div>
         </div>
     );
