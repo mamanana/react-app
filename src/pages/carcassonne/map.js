ceaf4cb1b9614a8c234f179d90cee3f9d4ce8584
hung <quochung@simicart.com>
2023-02-10 18:04:41 +0700

    update

--- src/pages/carcassonne/map.js
+++ src/pages/carcassonne/map.js
@@ -3,37 +3,35 @@ import React, { useEffect } from 'react';
 const TILE_WIDTH = 150;
 const TILE_HEIGHT = 150;
 
+let c = null;
+let canvas = null;
+
 function createImage(src) {
     const image = new Image();
     image.src = src;
     return image;
 }
 
+function resizeMap() {
+    const mapEle = document.getElementById('map');
+    if (canvas && mapEle) {
+        canvas.width = mapEle.offsetWidth;
+        canvas.height = innerHeight;
+    }
+}
+
 const Map = (props) => {
-    // const tiles = [
-    //     new
-    // ]
-
+    
     useEffect(() => {
-        const mapEle = document.getElementById('map');
-        const canvas = document.getElementById('carcassonne');
-        if (canvas && mapEle) {
-            const c = canvas.getContext('2d');
-            canvas.width = mapEle.offsetWidth;
-            canvas.height = innerHeight;
-
-            const startTile = createImage('/images/carcassonne/tiles/CastleWallRoad0.png');
-            startTile.onload = () => {
-                c.drawImage(startTile, canvas.width / 2 - 75, canvas.height / 2 - 75, TILE_WIDTH, TILE_HEIGHT);
-            };
-            // const startTiles = new Tiles({
-            //     position: { x: 40, y: 40 },
-            //     image: createImage('/images/carcassonne/tiles/CastleWallRoad0.png'),
-            // });
-            // startTiles.draw(c);
-            // default titles
-            // new Title({ x: })
+        canvas = document.getElementById('carcassonne');
+        if (canvas) {
+            c = canvas.getContext('2d');
         }
+        resizeMap();
+        window.addEventListener('resize', resizeMap);
+        return () => {
+            window.removeEventListener('resize', resizeMap);
+        };
     }, []);
 
     return (
